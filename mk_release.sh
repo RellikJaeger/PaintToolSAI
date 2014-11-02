ab521bffd39337ed08638cc5d31e3be9da739d2d
Mike Akiba <mike2718@gmail.com>
2014-11-02 21:38:47 +0800

    修正快照脚本

--- mk_release.sh
+++ mk_release.sh
@@ -3,4 +3,4 @@ then
 	echo "File PaintToolSAI.rar exists"
 	rm ../PaintToolSAI.rar
 fi
-rar a -m5 -rr ../PaintToolSAI.rar blotmap brushtex elemap papertex toolink toolnrm brushform.conf  brushtex.conf  language.conf  misc.ini  papertex.conf  presetcvsize.conf README.txt && rar c -zREADME.txt ../PaintToolSAI.rar
+rar a -m5 ../PaintToolSAI.rar blotmap brushtex elemap papertex toolink toolnrm brushform.conf  brushtex.conf  language.conf  misc.ini  papertex.conf  presetcvsize.conf README.txt && rar c -zREADME.txt ../PaintToolSAI.rar && rar rr ../PaintToolSAI.rar
